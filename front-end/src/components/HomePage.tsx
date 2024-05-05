dd844c1b886c5ebfee160660f5b5f14b3e3a719e
Sethu98 <sethu.kumar7@gmail.com>
2024-05-05 19:12:16 -0400

    Polish

--- front-end/src/components/HomePage.tsx
+++ front-end/src/components/HomePage.tsx
@@ -4,7 +4,7 @@ import {SERVER_URL} from "../constants";
 export function HomePage() {
     return (
         <div className={'txt-center'}>
-            <h1>Welcome</h1>
+            <h1>Welcome to One Drive Viewer</h1>
             <p>You need to login to microsoft to continue</p>
             <a href={SERVER_URL + '/login'}>Click to login</a>
         </div>
